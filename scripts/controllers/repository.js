32955c2d5df55f7900d47b8435cf5fc87000d70d
Felipe Skinner <felipeskinner@gmail.com>
2014-11-09 14:01:40 -0200

    JS refactoring

--- scripts/controllers/repository.js
+++ scripts/controllers/repository.js
@@ -1,58 +1,58 @@
-myApp.controller('repositoryController', function repositoryController($scope, githubService) {
+myApp.controller('repositoryController', function repositoryController($scope, githubFactory) {
+    'use strict';
+    var COMMIT_PAGE;
 
-	function init() {
-		$scope.notLoaded = true;
+    function init() {
+        $scope.notLoaded = true;
 
-		$scope.repositories = [];
-		$scope.commits = [];
+        $scope.repositories = [];
+        $scope.commits = [];
 
-		$scope.current_repository = {};
+        $scope.current_repository = {};
 
-		$scope.stars = 0;
-		$scope.forks = 0;
-		$scope.contribs = 0;
+        $scope.stars = 0;
+        $scope.forks = 0;
+        $scope.contribs = 0;
 
-		var COMMIT_PAGE = 1;
-		var current_repository;
-		getRepositories();
-	}
+        getRepositories();
+    }
 
-	$scope.loadRepositoryData = function (repo) {
-		$scope.notLoaded = false;
-		COMMIT_PAGE = 1;
-		$scope.current_repository = repo;
+    $scope.loadRepositoryData = function (repo) {
+        $scope.notLoaded = false;
+        COMMIT_PAGE = 1;
+        $scope.current_repository = repo;
 
-		$scope.stars = repo.stargazers_count;
-		$scope.forks = repo.forks_count;
-		
-		githubService.getContribCount(repo.name)
-			.then(function (response) {
-				$scope.contribs = response.data.length;
-			});
+        $scope.stars = repo.stargazers_count;
+        $scope.forks = repo.forks_count;
 
-		githubService.getCommits(repo.name, COMMIT_PAGE)
-			.then(function (response) {
-				$scope.commits = response.data;
-			});
-	};
+        githubFactory.getContribCount(repo.name)
+            .then(function (response) {
+                $scope.contribs = response.data.length;
+            });
 
-	$scope.loadMoreCommits = function () {
-		githubService.getCommits($scope.current_repository.name, COMMIT_PAGE)
-			.then(function (response) {
-				angular.forEach(response.data, function (value) {
-					$scope.commits.push(value);
-				});
-			});
+        githubFactory.getCommits(repo.name, COMMIT_PAGE)
+            .then(function (response) {
+                $scope.commits = response.data;
+            });
+    };
 
-		COMMIT_PAGE += 1;
-	};
+    $scope.loadMoreCommits = function () {
+        githubFactory.getCommits($scope.current_repository.name, COMMIT_PAGE)
+            .then(function (response) {
+                angular.forEach(response.data, function (value) {
+                    $scope.commits.push(value);
+                });
+            });
 
-	function getRepositories () {
-		githubService.listRepositories()
-			.then(function (response) {
+        COMMIT_PAGE += 1;
+    };
+
+    function getRepositories() {
+        githubFactory.listRepositories()
+            .then(function (response) {
                 $scope.repositories = response.data;
-            });;
-	};
+            });
+    }
 
-	init();
+    init();
 });
---
+++ scripts/services/githubFactory.js
@@ -0,0 +1,22 @@
+myApp.factory('githubFactory', function ($http) {
+    'use strict';
+
+    return {
+        listRepositories: listRepositories,
+        getCommits: getCommits,
+        getContribCount: getContribCount
+    };
+
+    function listRepositories() {
+        return $http.get('https://api.github.com/orgs/elasticsearch/repos?page=1&per_page=10');
+    }
+
+    function getCommits(repository, page) {
+        return $http.get('https://api.github.com/repos/elasticsearch/' + repository + '/commits?page=' + page + '&per_page=20');
+    }
+
+    function getContribCount(repository) {
+        return $http.get('https://api.github.com/repos/elasticsearch/' + repository + '/contributors');
+    }
+
+});
